a2fc8513a7029032d59265c87425268df3a84ef2
Alex <Alehandr0@users.noreply.github.com>
2018-01-17 18:28:28 +0200

    Update reg.js

--- reg.js
+++ reg.js
@@ -1,13 +1,9 @@
 var n=["accountname", "email", "reenter_email", "password", "reenter_password"];
-var kol=[11, 9, 9, 11, 11];
-var min=[97,  48, 95,  47, 95,  47, 95, ];
-var max=[122, 57, 122, 57, 122, 57, 122,];
-/*	 a-z  0-9 _`az /09 _`az /09 _`az		if(96==ascii||47==ascii)ascii=95;	*/
+var kol=[11, 9, 9, 11, 11];var min=[97,  48, 95,  47, 95,  47, 95, ];var max=[122, 57, 122, 57, 122, 57, 122,];
 var r=["", "", "", "", Rnd(10, 99)+"_PASS-",], m="";
-
 function Rnd(min, max){return Math.floor(Math.random()*(max-min+1))+min}
 function Chr(i){1==i?i=0:i=Rnd(0,6);p=Rnd(min[i], max[i]);if(96==p||47==p)p=95;return String.fromCharCode(p);}
-function Cre(d){for(var p="", i=1, j=1; i<=d; i++, j=i){if(j==d)j=1;p+=Chr(j)};return p;}  /* p+="("+j+")"+Chr(j)+", "}; */
+function Cre(d){for(var p="", i=1, j=1; i<=d; i++, j=i){if(j==d)j=1;p+=Chr(j)};return p;}
 for(var i=0; i<=4; i++){if(3==i){for(var b=1; b<=5; min.pop(), max.pop(), b++){};}
 min.push(58, 33, 44,  94, 63);max.push(59, 34, 46,  96, 90);
 (2==i||4==i)?r[i-1]=r[i]+=Cre(kol[i]):r[i]=Cre(kol[i]);
