580561037694d1e576d29c8f6bfcc801904afde2
Alex <Alehandr0@users.noreply.github.com>
2018-01-17 18:27:33 +0200

    Update reg.js

--- reg.js
+++ reg.js
@@ -1,17 +1,17 @@
-var kol=[11, 9, 9, 15, 15];
+var n=["accountname", "email", "reenter_email", "password", "reenter_password"];
+var kol=[11, 9, 9, 11, 11];
 var min=[97,  48, 95,  47, 95,  47, 95, ];
 var max=[122, 57, 122, 57, 122, 57, 122,];
-var n=["accountname", "email", "reenter_email", "password", "reenter_password"];
-var r=[], m="";
+/*	 a-z  0-9 _`az /09 _`az /09 _`az		if(96==ascii||47==ascii)ascii=95;	*/
+var r=["", "", "", "", Rnd(10, 99)+"_PASS-",], m="";
+
 function Rnd(min, max){return Math.floor(Math.random()*(max-min+1))+min}
 function Chr(i){1==i?i=0:i=Rnd(0,6);p=Rnd(min[i], max[i]);if(96==p||47==p)p=95;return String.fromCharCode(p);}
-function Cre(d){for(var p="", i=1, j=1; i<=d; i++, j=i){if(j==d)j=1;p+=Chr(j)};return p;}
-for(var i=0; i<=4; i++){
-	if(3==i){for(var b=1; b<=5; min.pop(), max.pop(), b++){};}
-	min.push(48, 58, 33, 44,  94, 63);
-	max.push(57, 59, 34, 46,  96, 90);
-	(2==i||4==i)?r[i-1]=r[i]=Cre(kol[i]):r[i]=Cre(kol[i]);if(2==i){r[i]+="@mailforspam.com";m=r[i-1]; r[i-1]+="@mailforspam.com";}
-}
+function Cre(d){for(var p="", i=1, j=1; i<=d; i++, j=i){if(j==d)j=1;p+=Chr(j)};return p;}  /* p+="("+j+")"+Chr(j)+", "}; */
+for(var i=0; i<=4; i++){if(3==i){for(var b=1; b<=5; min.pop(), max.pop(), b++){};}
+min.push(58, 33, 44,  94, 63);max.push(59, 34, 46,  96, 90);
+(2==i||4==i)?r[i-1]=r[i]+=Cre(kol[i]):r[i]=Cre(kol[i]);
+if(2==i){r[i]+="@mailforspam.com";m=r[i-1]; r[i-1]+="@mailforspam.com";}}
 for(var i=0; i<=4; i++){document.getElementById(n[i]).value=r[i]}
 document.getElementById('i_agree_check').checked = true;
 alert("m = "+m+"\n"+r);
