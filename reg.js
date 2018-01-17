ea9318a379d2412db6a472d997d72bc2eef30451
Alex <Alehandr0@users.noreply.github.com>
2018-01-17 10:28:53 +0200

    Update reg.js

--- reg.js
+++ reg.js
@@ -5,7 +5,7 @@ var n=["accountname", "email", "reenter_email", "password", "reenter_password"];
 var r=[], m="";
 function Rnd(min, max){return Math.floor(Math.random()*(max-min+1))+min}
 function Chr(i){1==i?i=0:i=Rnd(0,6);p=Rnd(min[i], max[i]);if(96==p||47==p)p=95;return String.fromCharCode(p);}
-function Cre(d){for(var p="", i=1, j=1; i<=d; i++, j=i){if(j==d)j=1;p+=Chr(j)};return p;}		/*	p+="("+j+")"+Chr(j)+", "};	*/
+function Cre(d){for(var p="", i=1, j=1; i<=d; i++, j=i){if(j==d)j=1;p+=Chr(j)};return p;}
 for(var i=0; i<=4; i++){
 	if(3==i){for(var b=1; b<=5; min.pop(), max.pop(), b++){};}
 	min.push(48, 58, 33, 44,  94, 63);
