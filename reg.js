e2fd6ae865bd1de610ea69fb63e5fdac1ddfb2d2
Alex <Alehandr0@users.noreply.github.com>
2018-01-17 10:14:32 +0200

    Update reg.js

--- reg.js
+++ reg.js
@@ -1,15 +1,18 @@
-var min=[97,  48, 58, 33, 44,  94,];
-var max=[122, 57, 59, 34, 46,  96,];
-var P0R0L="", dl=10;
+var kol=[11, 9, 9, 15, 15];
+var min=[97,  48, 95,  47, 95,  47, 95, ];
+var max=[122, 57, 122, 57, 122, 57, 122,];
+var n=["accountname", "email", "reenter_email", "password", "reenter_password"];
+var r=[], m="";
 function Rnd(min, max){return Math.floor(Math.random()*(max-min+1))+min}
-function FRndChr(i){1==i?i=0:i=Rnd(0,5);return String.fromCharCode(Math.floor(Math.random()*(max[i]-min[i]+1))+min[i]);}
-for(var i=1, j=1; i<=dl; i++, j=i){if(j==dl)j=1;P0R0L+=FRndChr(j)};
-
-document.getElementById('accountname').value=P0R0L;
-document.getElementById('password').value=P0R0L;
-document.getElementById('reenter_password').value=P0R0L;
-document.getElementById('email').value=P0R0L+"@mailforspam.com";
-document.getElementById('reenter_email').value=P0R0L+"@mailforspam.com";
+function Chr(i){1==i?i=0:i=Rnd(0,6);p=Rnd(min[i], max[i]);if(96==p||47==p)p=95;return String.fromCharCode(p);}
+function Cre(d){for(var p="", i=1, j=1; i<=d; i++, j=i){if(j==d)j=1;p+=Chr(j)};return p;}		/*	p+="("+j+")"+Chr(j)+", "};	*/
+for(var i=0; i<=4; i++){
+	if(3==i){for(var b=1; b<=5; min.pop(), max.pop(), b++){};}
+	min.push(48, 58, 33, 44,  94, 63);
+	max.push(57, 59, 34, 46,  96, 90);
+	(2==i||4==i)?r[i-1]=r[i]=Cre(kol[i]):r[i]=Cre(kol[i]);if(4==i){r[i]+="@mailforspam.com";m=r[i-1]; r[i-1]+="@mailforspam.com";}
+}
+for(var i=0; i<=4; i++){document.getElementById(n[i]).value=r[i]}
 document.getElementById('i_agree_check').checked = true;
-
-window.open("https://www.mfsa.info/mail/"+P0R0L+"/1");
+alert("m = "+m+"\n"+r);
+window.open("https://www.mfsa.info/mail/"+m+"/1");
