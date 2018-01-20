7371fca15167556c42f92f52ce64b4d8d1227594
Alex <Alehandr0@users.noreply.github.com>
2018-01-20 09:26:59 +0200

    Update reg.js

--- reg.js
+++ reg.js
@@ -3,7 +3,7 @@ var kol=[11, 9, 9, 11, ];var min=[97, 48, 95, 47, 95, 47, 95, ];var max=[122, 57
 var r=[], m='';
 function Rnd(min, max){return Math.floor(Math.random()*(max-min+1))+min}
 function Chr(i){1==i?i=0:i=Rnd(0,6);p=Rnd(min[i], max[i]);if(96==p||47==p)p=95;return String.fromCharCode(p);}
-function Chz(i){p=Rnd(min[i], max[i]);return String.fromCharCode(p);}
+function Chz(a){p=Rnd(min[a], max[a]);return String.fromCharCode(p);}
 function Cre(d){for(var p='', i=1, j=1; i<=d; i++, j=i){if(j==d)j=1;p+=Chr(j)};return p;}
 for(var i=0; i<=3; i++){if(3==i){for(var b=1; b<=5; min.pop(), max.pop(), b++){};
 min.push(58, 33, 44, 94, 63, 65,);max.push(59, 34, 46, 96, 90, 90,);}
